8d7cdf639460b4dd219b407486d29750c746b1e2
Gabriel <glumertz10@gmail.com>
2021-12-23 00:15:23 -0300

    parte 5

--- script.js
+++ script.js
@@ -40,10 +40,20 @@ function update(event){
 }
 
 function startGame() {
+
+    
+
     if(snake[0].x > 16 * box && direction == "right") snake[0].x = 0
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
     if(snake[0].y > 16 * box && direction == "down") snake[0].y = 0
     if(snake[0].y < 0 && direction == "left") snake[0].y = 16 * box
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game)
+            alert("Game Over :(")
+        }
+    }
     
     newBG()
     newSnake()
@@ -57,7 +67,14 @@ function startGame() {
     if(direction == "up") snakeY -= box
     if(direction == "down") snakeY += box
 
-    snake.pop()
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop()
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+
+    }
+    
 
     let newHead = {
         x: snakeX,
